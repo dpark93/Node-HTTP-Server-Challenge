8bfffa339a356cd5a200f42541651474611e3c65
dpark93 <d.park.official@gmail.com>
2023-11-07 09:39:03 -0700

    complete

---
+++ server.js
@@ -0,0 +1,9 @@
+const { split } = require('lodash'); const http = require('node:http'); let HOST = "localhost"; let PORT = 3000;
+const server = http.createServer((request, response) => {
+    let url = request.url.split('/');
+    if (url[1] === "" ) {
+      if (request.method === 'GET') response.end("Hello, Galvanize!");
+    }
+    response.statusCode = 404; response.end();
+  });
+  server.listen(PORT, HOST, () => {console.log(`Server running at ${PORT}:${HOST}`);});
